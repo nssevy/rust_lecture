5369419a1b8154da18ce0f9ac3dd88c3b4a5e7a0
Yves <sevyns.pro@gmail.com>
2026-09-13 16:52:48 +0200

    State : Sorti du tuple de la fn qui accepte les numero qui ne sont pas des nbr a fix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 use std::env;
-use std::fs;
+//use std::fs;
+
+fn choose_dict_and_num(arguments: &[String]) -> Result<(&str, &str), String> {
+    
+    let (dict, numero) = match arguments {
+        [numero] => ("numbers.dict" ,numero.as_str()),
+        [dict, numero] => (dict.as_str() ,numero.as_str()),
+        _ => return Err("Dict error".to_string())
+    };
+
+    /*let read = match fs::read_to_string(dict) {
+        Ok(fichier) => fichier,
+        Err(_) => return Err(format!("Dict Error")),
+    };*/
+
+    Ok((dict, numero))
+}
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
+
+    match choose_dict_and_num(&args){
+        Ok((dict, numero)) => println!("Dictionnaire : {} (--) lignes \nNombre : {}", dict, numero),
+        Err(e)=> eprintln!("erreur : {}", e),
+    }
     //dbg!(&args);
 
-    let (dict, numero) = match args.as_slice() {
+    /*let (dict, numero) = match args.as_slice() {
         [numero] => ("numbers.dict" ,numero.as_str()),
         [dict, numero] => (dict.as_str() ,numero.as_str()),
         _ => return eprintln!("Error")
@@ -19,14 +40,25 @@ fn main() {
     let lignes = f.lines().count();
 
     println!("Dictionnaire : {} ({} lignes)", dict, lignes);
-    println!("Nombre : {}", numero);
+    println!("Nombre : {}", numero);*/
 }
 
 #[cfg(test)]
 
 mod tests {
 
-    use super::*; 
+    use super::*;
+
+    #[test]
+    fn check_que_le_dictionnaire_est_bien_lu() {
+        asse
+    }
+
+    #[test]
+    fn check_que_1_argument_renvoi_bien_le_dictionnaire_et_nombre(){
+        let tab: Vec<String> = vec!["42".into()];
+        assert_eq!( Ok( ("numbers.dict", "42" ) ), choose_dict_and_num(&tab));
+    }
 
 }
 
